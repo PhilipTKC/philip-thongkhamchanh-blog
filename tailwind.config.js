c9b6fd9c4f795ec92f80a986ec2aa1d7a995c5a7
Philip <philip+github@thongkhamchanh.me>
2020-10-05 01:06:42 +1300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   purge: {
     enabled: process.env.NODE_ENV === "production",
-    content: ["./src/**/*.html", "./src/**/*.ts"],
+    content: ["./src/**/*.html", "./src/**/*.ts", "./src/**/*.md"],
   },
   theme: {
     fontWeight: {
