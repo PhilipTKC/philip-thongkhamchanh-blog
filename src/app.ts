d2407dc9aba7bd3d60ff4a43c34054fd431651ed
Philip <philip+github@thongkhamchanh.me>
2020-10-09 01:07:14 +1300

    Add dark theme

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,50 @@ import { IRouter, IViewModel } from "aurelia";
 import { hooks } from "router-hooks";
 import { routes } from "routes";
 
-import(/* webpackPrefetch: true */ "./css/output.css");
-import(/* webpackPrefetch: true */ "./css/nprogress.css");
-
 export class App implements IViewModel {
   private static routes = routes;
 
+  private isDark: boolean;
+
   constructor(@IRouter private router: IRouter) {}
 
-  afterBind(): void {
+  async afterBind(): Promise<void> {
     this.router.addHook(hooks.navigation.f);
     this.router.addHook(hooks.title.f, hooks.title.options);
+
+    await this.loadStyles();
+  }
+
+  async loadStyles(): Promise<void> {
+    await import("./css/output.css");
+    await import("./css/nprogress.css");
+
+    const isDark = localStorage.getItem("isDark");
+
+    if (isDark === "true") {
+      this.isDark = true;
+      this.loadDarkTheme();
+    }
+  }
+
+  loadDarkTheme(): void {
+    const headElement = document.getElementsByTagName("head")[0];
+    const linkElement: HTMLLinkElement = document.createElement("link");
+    linkElement.id = "dark";
+    linkElement.rel = "stylesheet";
+    linkElement.type = "text/css";
+    linkElement.href = "./dark.css";
+    headElement.appendChild(linkElement);
+    this.isDark = true;
+    localStorage.setItem("isDark", "true");
+  }
+
+  removeDarkTheme(): void {
+    const style = document.getElementById("dark");
+    if (style !== null) {
+      style.remove();
+      this.isDark = false;
+      localStorage.setItem("isDark", "false");
+    }
   }
 }
