ea384d0a7795467b7170520efe8ce20a506d1c55
PhilipTKC <philip+github@thongkhamchanh.me>
2021-01-17 15:47:42 +1300

    Fix time-to-read value converter

--- src/value-converters/time-to-read.ts
+++ src/value-converters/time-to-read.ts
@@ -1,5 +1,5 @@
 export class TimeToReadValueConverter {
   toView(inputLength: number): string {
-    return `${Math.ceil(inputLength / 250)} min read`;
+    return `${Math.trunc(Math.floor(inputLength / 300) * .60)} min read`;
   }
 }
